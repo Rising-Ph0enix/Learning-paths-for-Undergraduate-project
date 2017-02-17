3d3c7e96b61c795bdaa01d27693269b6ca868dc8
Ganesh <chessganesh@gmail.com>
2017-02-17 16:15:31 +0530

    Create Learn JS & node.js

---
+++ Cloud PaaS Server/Learn JS & node.js
@@ -0,0 +1,22 @@
+1) Basics
+
+http://www.nodebeginner.org/#intended-audience
+
+2) Practical experience
+
+https://www.codecademy.com/courses/getting-started-v2/0/1?curriculum_id=550af85efed2a8c613000001
+
+https://www.airpair.com/javascript/node-js-tutorial (some concepts)
+
+3) Fundamentals of node.js
+
+file:///C:/Users/13cse11/Downloads/javascript_the_good_parts.pdf (your local version)
+
+
+Course:
+
+https://www.quora.com/What-is-the-best-Node-js-course-online
+
+https://www.udemy.com/node-js-training-and-fundamentals/
+
+https://www.lynda.com/Nodejs-tutorials/Running-Node-script/101554/110736-4.html?srchtrk=index%3a0%0alinktypeid%3a2%0aq%3anode.js%0apage%3a1%0as%3arelevance%0asa%3atrue%0aproducttypeid%3a2
